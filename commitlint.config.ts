18ca7f5b0a6a7b61ea843e2c1b220424c9887ad3
SHIN <akiyachiumi@gmail.com>
2025-03-22 13:47:48 +0800

    chore: install commitlint and husky

---
+++ commitlint.config.ts
@@ -0,0 +1,10 @@
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'refactor']],
+  },
+};
+
+export default config;
